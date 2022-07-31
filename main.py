3424485aaa2e88c154c788d91a08542d9ce5083a
Thanakorn Doakkiang <thanakorn.dkg@hotmail.com>
2022-07-31 19:02:15 +1200

    adding main_PySimpleGUI + user.config

--- main.py
+++ main.py
@@ -866,79 +866,12 @@ button = Button(text="Submit",command=selectTown).grid(row=1,column=1)
 
 root.mainloop()
 """
-
-#การเรียกใช้ Combo Box
+#
 
 from tkinter import *
-from tkinter import ttk ###################################
 
 root = Tk()
 root.title("JustPython")
 root.geometry("500x500+0+0")
 
-def selectTown():
-    Label(text=choice.get()+" selected").grid(row=2,column=0)
-
-Label(text="Address").grid(row=0,column=0)
-choice = StringVar(value="Select town") #set default
-combo = ttk.Combobox(textvariable=choice)
-combo["value"] = ("Manurewa","East Tamaki","Mout Wellington")
-combo.grid(row=0,column=1)
-button = Button(text="Submit",command=selectTown).grid(row=1,column=1)
-
 root.mainloop()
-
-
-
-"""
-PySimpleGUI Example 1 - The One-Shot Window
-This type of program is called a "one-shot" window because the window is displayed one time,
-the values collected, and then it is closed. It doesn't remain open for a long time like you would in a Word Processor.
-
-import PySimpleGUI as sg                        # Part 1 - The import
-
-# Define the window's contents
-layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
-            [sg.Input()],
-            [sg.Button('Ok')] ]
-
-# Create the window
-window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
-
-# Display and interact with the Window
-event, values = window.read()                   # Part 4 - Event loop or Window.read call
-
-# Do something with the information gathered
-print('Hello', values[0], "! Thanks for trying PySimpleGUI")
-
-# Finish up by removing from the screen
-window.close()                                  # Part 5 - Close the Window
-*************************************
-PySimpleGUI Example 2 - Interactive Window
-In this example, our window will remain on the screen until the user closes the window or clicks the Quit button.
-The main difference between the one-shot window you saw earlier and an interactive window is the addition of an "Event Loop".
-The Event Loop reads events and inputs from your window. The heart of your application lives in the event loop.
-
-import PySimpleGUI as sg
-
-# Define the window's contents
-layout = [[sg.Text("What's your name?")],
-          [sg.Input(key='-INPUT-')],
-          [sg.Text(size=(40,1), key='-OUTPUT-')],
-          [sg.Button('Ok'), sg.Button('Quit')]]
-
-# Create the window
-window = sg.Window('Window Title', layout)
-
-# Display and interact with the Window using an Event Loop
-while True:
-    event, values = window.read()
-    # See if user wants to quit or window was closed
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        break
-    # Output a message to the window
-    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
-
-# Finish up by removing from the screen
-window.close()
-"""
