c1702c30ce9b425adc38510bf0771f3fb253c456
Thanakorn Doakkiang <thanakorn.dkg@hotmail.com>
2022-07-31 12:37:51 +1200

    Finish learn Tkinter

--- main.py
+++ main.py
@@ -689,8 +689,8 @@ def selectFile():
 button = Button(text="Select file",command=selectFile).pack()
 
 root.mainloop()
-"""
-#การเรียกใช้ tkinter กดปุ่มเพื่อเรียกหน้าต่างเลือกไฟล์
+*****************************************
+#การเรียกใช้ tkinter กดปุ่มเพื่อเรียกหน้าต่างเลือกไฟล์ และอ่านข้อมูลไปแสดงในหน้าต่าง
 
 from tkinter import *
 from tkinter.filedialog import * #################################
@@ -701,13 +701,192 @@ root.geometry("500x500+0+0")
 
 def selectFile():
     file = askopenfilename()
-    myLabel = Label(text=file).pack()
+    fileData = open(file)
+    myLabel = Label(text=fileData.read()).pack()
 
 button = Button(text="Select file",command=selectFile).pack()
 
 root.mainloop()
+*****************************************
+#การเรียกใช้ radio button เลือกภาษา และสั่งให้มี dialog
 
+from tkinter import *
+import tkinter.messagebox #########################
 
+root = Tk()
+root.title("JustPython")
+root.geometry("500x500+0+0")
+
+def showChoice():
+    choice = language.get()
+    if choice == 1:
+        Label(text="Python selected").grid(row=0,column=4) #ใช้ pack ไม่ได้ เพราะ radio button ถูก assign เป็น grid
+        tkinter.messagebox.showinfo("Comfirmed","Python selected")
+    elif choice == 2:
+        Label(text="JAVA selected").grid(row=0,column=6)
+        tkinter.messagebox.showinfo("Comfirmed","JAVA selected")
+    elif choice == 3:
+        Label(text="PHP selected").grid(row=0,column=6)
+        tkinter.messagebox.showinfo("Comfirmed","PHP selected")
+    else:
+        Label(text="C++ selected").grid(row=0,column=6)
+        tkinter.messagebox.showinfo("Comfirmed","C++ selected")
+
+language = IntVar()
+language.set(0) #set default value
+
+Radiobutton(text="Python",variable=language,value=1,command=showChoice).grid(row=0, column=0)
+Radiobutton(text="JAVA",variable=language,value=2,command=showChoice).grid(row=0, column=1)
+Radiobutton(text="PHP",variable=language,value=3,command=showChoice).grid(row=0, column=2)
+Radiobutton(text="C++",variable=language,value=4,command=showChoice).grid(row=0, column=3)
+
+root.mainloop()
+*********************************************
+#การเรียกใช้ Checkbutton (มีค่า 0,1) เลือกภาษา และแสดงค่าที่ถูกเลือก
+
+from tkinter import *
+
+root = Tk()
+root.title("JustPython")
+root.geometry("500x500+0+0")
+
+def showChoice():
+    print(language1.get(),language2.get(),language3.get(),language4.get())
+    language_set = str(language1.get())+str(language2.get())+str(language3.get())+str(language4.get())
+    Label(text=language_set).pack(anchor=W)
+
+language1 = IntVar()
+language2 = IntVar()
+language3 = IntVar()
+language4 = IntVar()
+
+Checkbutton(text="Python",variable=language1).pack(anchor=W) #anchor ระบุั่งที่จะให้แสดงผล (North,South,West,East)
+Checkbutton(text="JAVA",variable=language2).pack(anchor=W)
+Checkbutton(text="PHP",variable=language3).pack(anchor=W)
+Checkbutton(text="C++",variable=language4).pack(anchor=W)
+
+Button(text="Show choice",command=showChoice).pack(anchor=W)
+
+root.mainloop()
+*****************************
+#การเรียกใช้ Checkbutton (มีค่า 0,1) เลือกภาษา และนำค่าที่ได้ไปสั่งงานต่อ
+
+from tkinter import *
+
+root = Tk()
+root.title("JustPython")
+root.geometry("500x500+0+0")
+
+def showChoice():
+    choice1 = language1.get()
+    choice2 = language2.get()
+    choice3 = language3.get()
+    choice4 = language4.get()
+
+    if choice1 == 1:
+        Label(text="Python selected").pack()
+    if choice2 == 1:
+        Label(text="JAVA selected").pack()
+    if choice3 == 1:
+        Label(text="PHP selected").pack()
+    if choice4 == 1:
+        Label(text="C++ selected").pack()
+
+language1 = IntVar()
+language2 = IntVar()
+language3 = IntVar()
+language4 = IntVar()
+
+Checkbutton(text="Python",variable=language1).pack(anchor=W) #anchor ระบุั่งที่จะให้แสดงผล (North,South,West,East)
+Checkbutton(text="JAVA",variable=language2).pack(anchor=W)
+Checkbutton(text="PHP",variable=language3).pack(anchor=W)
+Checkbutton(text="C++",variable=language4).pack(anchor=W)
+
+Button(text="Show choice",command=showChoice).pack(anchor=W)
+
+root.mainloop()
+********************************
+#การเรียกใช้ Entry box ที่มีค่า default และมีปุ่ม reset ค่าทั้งหมด
+
+from tkinter import *
+
+root = Tk()
+root.title("JustPython")
+root.geometry("500x500+0+0")
+
+Label(text="Firstname").grid(row=0)
+Label(text="Surname").grid(row=1)
+Label(text="Company").grid(row=2)
+Label(text="Salary").grid(row=3)
+
+input_firstname = Entry()
+input_firstname.grid(row=0,column=1)
+input_firstname.insert(0,"Thanakorn") #set default value
+
+input_surname = Entry()
+input_surname.grid(row=1,column=1)
+input_surname.insert(0,"Doakkiang")
+
+input_company = Entry()
+input_company.grid(row=2,column=1)
+input_company.insert(0,"Datamars")
+
+input_salary = Entry()
+input_salary.grid(row=3,column=1)
+input_salary.insert(0,"4500")
+
+def reset_data():
+    input_firstname.delete(0,END)
+    input_surname.delete(0,END)
+    input_company.delete(0,END)
+    input_salary.delete(0,END)
+
+Button(text="Reset all data",command=reset_data).grid(row=4)
+
+root.mainloop()
+*****************************************
+#การเรียกใช้ Combo Box
+
+from tkinter import *
+from tkinter import ttk ###################################
+
+root = Tk()
+root.title("JustPython")
+root.geometry("500x500+0+0")
+
+def selectTown():
+    Label(text=choice.get()+" selected").grid(row=2,column=0)
+
+Label(text="Address").grid(row=0,column=0)
+choice = StringVar(value="Select town")
+combo = ttk.Combobox(textvariable=choice)
+combo["value"] = ("Manurewa","East Tamaki","Mout Wellington")
+combo.grid(row=0,column=1)
+button = Button(text="Submit",command=selectTown).grid(row=1,column=1)
+
+root.mainloop()
+"""
+
+#การเรียกใช้ Combo Box
+
+from tkinter import *
+from tkinter import ttk ###################################
+
+root = Tk()
+root.title("JustPython")
+root.geometry("500x500+0+0")
+
+def selectTown():
+    Label(text=choice.get()+" selected").grid(row=2,column=0)
+
+Label(text="Address").grid(row=0,column=0)
+choice = StringVar(value="Select town") #set default
+combo = ttk.Combobox(textvariable=choice)
+combo["value"] = ("Manurewa","East Tamaki","Mout Wellington")
+combo.grid(row=0,column=1)
+button = Button(text="Submit",command=selectTown).grid(row=1,column=1)
+
+root.mainloop()
 
 
 
